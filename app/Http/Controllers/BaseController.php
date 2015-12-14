65f7ef91d1aafd68c146fbe58b975853e310ba41
Javed Ahamad <javed@scaledesk.com>
2015-12-14 11:59:29 +0530

    edited base controller

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Javed
+ * Date: 11/12/15
+ * Time: 5:11 PM
+ */
 namespace App\Http\Controllers;
 use App\libraries\Messages;
 use Illuminate\Support\Facades\Input;
@@ -19,43 +25,13 @@ class BaseController extends Controller
 
     }
     public function success($message='success',$status_code=200){
-        /*return $this->response()->array([
-            'message'=>$message,
-            'status_code'=>$status_code
-        ])->statusCode($status_code);*/
         return [
           'message' =>$message,
             'status_code' =>$status_code
         ];
     }
 
-    /*public function successWithData($message='success',$status_code=200){
-        if($message==""){
-            $message="success";
-        }
-        if($status_code==""){
-            $status_code=200;
-        }
-        return $this->response()->array([
-            'message'=>$message,
-            'status_code'=>$status_code,
-            key(func_get_arg(2))=>func_get_arg(2)[key(func_get_arg(2))]
-        ])->statusCode($status_code);
 
-    }
-    public function errorWithData($message='error',$status_code=404){
-        if($message==""){
-            $message="error";
-        }
-        if($status_code==""){
-            $status_code=404;
-        }
-        return $this->response()->array([
-            'message'=>$message,
-            'status_code'=>$status_code,
-            key(func_get_arg(2))=>func_get_arg(2)[key(func_get_arg(2))]
-        ])->statusCode($status_code);
-    }*/
     /**
      * error response method with default message and error code
      */
@@ -64,28 +40,5 @@ class BaseController extends Controller
           'message'=>$message,
             'status_code'=>$status_code
         ];
-
-
-            /*$this->response()->array([
-            'message'=>$message,
-            'status_code'=>$status_code
-        ])->statusCode($status_code);*/
     }
-
-   /* public function my_validate($data_array){
-        $validate=Validator::make($data_array['data'],$data_array['rules'],$data_array['messages']);
-        if($validate->fails()){
-            return ['result'=>false,'error'=>$this->error(Messages::showErrorMessages($validate),422)];
-        }else{
-            return ['result'=>true,'error'=>'no error'];
-        }
-    }
-
-    protected function respondWithCursor($collection, $callback, CursorInterface $cursor)
-    {
-        $resource = new Collection($collection, $callback);
-        $resource->setCursor($cursor);
-        $rootScope = $this->fractal->createData($resource);
-        return $this->respondWithArray($rootScope->toArray());
-    }*/
 }
